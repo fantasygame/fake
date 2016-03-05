a7db006fa16de0baa782a8a09ca4ae81f38e7f43
Kamil <kamilgrochola@gmail.com>
2016-03-05 15:44:37 +0100

    Migration for uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305133600) do
+ActiveRecord::Schema.define(version: 20160305144147) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160305133600) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "role"
+    t.string   "Image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
